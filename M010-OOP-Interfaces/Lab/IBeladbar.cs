82f57f349c7bd94ea75d4638b93483745f1d79f8
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-10-30 14:22:41 +0100

    M010 Lab Schiff beladen

---
+++ M010-OOP-Interfaces/Lab/IBeladbar.cs
@@ -0,0 +1,12 @@
+﻿namespace Fahrzeugpark
+{
+    // Aufgabe 1: Interface erstellen
+    public interface IBeladbar
+    {
+        Fahrzeug Ladung { get; set; }
+
+        bool Belade(Fahrzeug fahrzeug);
+
+        Fahrzeug Entlade();
+    }
+}
